92a5b95e736ca1ed6b57e231f07a83e9e58ed542
Nigel Wilson <npwilson2490@hotmail.co.uk>
2021-11-16 23:30:18 +0000

    Improved formatting for error messages on ship placement

--- Battleships/Human.cpp
+++ Battleships/Human.cpp
@@ -36,7 +36,7 @@ void Human::addShips()
             std::string input;
             std::cin >> input;
 
-            std::cout << "You entered: " << input << std::endl;
+            std::cout << std::endl;
 
             if (!std::regex_match(input, inputRegex))
             {
